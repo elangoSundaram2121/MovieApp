41e918846c66384c5f00877030716347b5b042a8
Elango Sundaram <ESundaram@victoria.com>
2023-02-03 16:13:35 +0530

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by esundaram esundaram on 03/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
